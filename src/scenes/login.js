724490dddf67dd9b8d779c3cc699e12a1ad2facc
yamad07 <yuhsukeshootsfc@gmail.com>
2017-12-21 19:57:17 +0900

    [change] loginとsignupのrender変更

--- src/scenes/login.js
+++ src/scenes/login.js
@@ -5,8 +5,12 @@ import {
     Text,
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import Orientation from 'react-native-orientation';
 
 export default class Login extends Component{
+    componentWillMount() {
+        Orientation.lockToPortrait();
+    }
     render(){
         return(
             <View>
--- src/scenes/sign_up.js
+++ src/scenes/sign_up.js
@@ -5,7 +5,13 @@ import {
     Text,
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import Orientation from 'react-native-orientation';
+
+
 export default class SignUp extends Component{
+    componentWillMount() {
+        Orientation.lockToPortrait();
+    }
     render(){
         return(
             <View>
