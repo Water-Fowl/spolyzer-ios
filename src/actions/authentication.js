dd364ff3f79c65ad195dc1f35597c2c1111f584e
yamad07 <yuhsukeshootsfc@gmail.com>
2017-12-30 17:30:21 +0900

    [change] linkではなくパブリックのIPアドレスに変更したら動いたので、そちらを修正

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -1,7 +1,7 @@
 import { CALL_API } from 'redux-api-middleware'
 import { Actions } from 'react-native-router-flux';
 
-const BASE_URL = 'http://ec2-52-198-150-98.ap-northeast-1.compute.amazonaws.com/api/v1';
+const BASE_URL = "http://52.198.150.98/api/v1";
 export const REQUEST = "REQUEST"
 export const RECIEVED = "RECIEVED"
 
@@ -9,7 +9,7 @@ export const RECIEVED = "RECIEVED"
 export function postUserAuthentication(body){
   return dispatch => {
     dispatch(requestAuthentication(body));
-    return fetch(BASE_URL + '/auth', {
+    return fetch(BASE_URL + "/auth/sign_in", {
       method: 'POST',
       headers:{
         'Accept': 'application/json',
@@ -18,7 +18,7 @@ export function postUserAuthentication(body){
       body: JSON.stringify(body)
     })
     .then(response => response.json())
-    .then(json => console.log(json.status))
+    .then(json => console.log(json))
     .then(dispatch(receivedAuthentication(true)))
     .catch(function(error){
       console.log(error.message)
