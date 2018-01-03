1ec0c886bc48db7fa553915411f5df35e3c1356f
maysay526 <maysay526@gmail.com>
2018-01-04 00:28:28 +0900

    [add]試合形式の列を作成

--- src/scenes/analysis_create.js
+++ src/scenes/analysis_create.js
@@ -26,10 +26,22 @@ export default class AnalysisCreate extends React.Component{
                     検索条件
                 </Text>
 
-                <Text style={styles.game_style_text}>
-                    試合形式
-                </Text>
 
+                <View style={{flexDirection:"row"}}>
+                    <Text style={styles.game_style_text}>
+                    試合形式
+                    </Text>
+                    <View style={styles.frame}>
+                        <Text style={styles.singles_text}>
+                            シングルス 
+                         </Text>
+                         <Text style={styles.doubles_text}>
+                           ダブルス
+                         </Text>
+                    </View>
+                </View>
+
+             
             </View>
 
 
@@ -64,12 +76,62 @@ const styles = StyleSheet.create({
     
     game_style_text: {
         color: '#ffffff',
-        fontSize: 16,
-        marginTop: 42,
+        fontSize: 15,
+        marginTop: 45,
         marginLeft: 40,
         backgroundColor: 'transparent',
         fontWeight: 'bold',
         alignSelf: 'flex-start',
         
      },
+
+     frame: {
+        flexDirection: "row",
+        backgroundColor: 'transparent',
+        borderRightColor: '#0a2444',
+        borderTopColor: '#0a2444',
+        borderLeftColor: '#0a2444',
+        borderBottomColor: '#0a2444',
+        height: 34,
+        width: 222,
+        borderWidth: 1.5,
+        marginLeft: 30,
+        borderRadius: 3,
+        marginTop: 37,
+
+     },
+
+      singles_text: {
+        color: '#ffffff',
+        fontSize: 15,
+        backgroundColor: 'transparent',
+        fontWeight: 'bold',
+        alignSelf: 'flex-start',
+        paddingTop: 5,
+        paddingLeft: 14,
+        paddingBottom: 5,
+        paddingRight: 14,
+        marginLeft: 10,
+        marginRight: 6,
+        marginTop: 2,
+        marginBottom: 3,
+        
+     },
+
+      doubles_text: {
+        color: '#ffffff',
+        fontSize: 15,
+        backgroundColor: 'transparent',
+        fontWeight: 'bold',
+        alignSelf: 'flex-start',
+        paddingTop: 5,
+        paddingLeft: 17,
+        paddingBottom: 5,
+        paddingRight: 17,
+        marginTop: 2,
+        marginBottom: 3,
+        
+     },
+
+
 });
