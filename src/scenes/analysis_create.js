f0e5ba1da7540d192c7e7962ac977171651883d8
maysay526 <maysay526@gmail.com>
2018-01-03 23:53:38 +0900

    [add]analyz画面の背景及び簡単なテキストの追加

--- src/scenes/analysis_create.js
+++ src/scenes/analysis_create.js
@@ -10,6 +10,7 @@ import {
 } from "react-native";
 import { connect } from 'react-redux';
 import Orientation from 'react-native-orientation';
+import { Background } from "../components";
 
 export default class AnalysisCreate extends React.Component{
     componentWillMount() {
@@ -17,11 +18,58 @@ export default class AnalysisCreate extends React.Component{
     }
     render(){
         return(
-            <View/>
+            <View style={styles.container}>
+
+               <Background/>
+
+               <Text style={styles.subtitle_text}>
+                    検索条件
+                </Text>
+
+                <Text style={styles.game_style_text}>
+                    試合形式
+                </Text>
+
+            </View>
+
+
+
+
+
+
+
+
+
+        
         );
     }
 }
 
 const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+     },
+
+    subtitle_text: {
+        color: '#ffffff',
+        fontSize: 22,
+        textAlign: 'center',
+        alignSelf: 'center',
+        marginTop: 66,
+        backgroundColor: 'transparent',
+        fontWeight: 'bold',
+        
+     },
     
+    game_style_text: {
+        color: '#ffffff',
+        fontSize: 16,
+        marginTop: 42,
+        marginLeft: 40,
+        backgroundColor: 'transparent',
+        fontWeight: 'bold',
+        alignSelf: 'flex-start',
+        
+     },
 });
