925b071e09e0771c4f423d241885768915e12666
yamad07 <yuhsukeshootsfc@gmail.com>
2018-01-05 16:24:47 +0900

    [remove] 余分な行末のスペースを消した

--- src/components/field_inside.js
+++ src/components/field_inside.js
@@ -3,32 +3,31 @@ import { Dimensions,StyleSheet,Image, Text, View, TouchableHighlight } from 'rea
 
 
 export class FieldInSide extends React.Component{
-    
+
     constructor(props) {
         super(props);
-    }      
+    }
 
     render(){
-        const {actions, positions, this_position, setParams, side} = this.props; 
+        const {actions, positions, this_position, setParams, side} = this.props;
         return(
             <View style={ styles.container }>
                 <TouchableHighlight onPress={() =>{
-                    this.props.setModalVisible(true) 
+                    this.props.setModalVisible(true)
                     this.props.setParams({ this_position: this.props.this_position, this_side: side });
-                }}> 
+                }}>
                     <View style={{width:40, height:19, backgroundColor:"rgba(46, 167, 224, 0.5)"}}/>
                 </TouchableHighlight>
-                <TouchableHighlight onPress={() =>{ 
-                    this.props.setModalVisible(true) 
+                <TouchableHighlight onPress={() =>{
+                    this.props.setModalVisible(true)
                     this.props.setParams({ this_position: this.props.this_position + 3, this_side: side });
-                }}> 
+                }}>
                     <View style={{width:28, height:28, borderRadius: 100/2, backgroundColor:"rgba(46, 167, 224, 0.5)"}}/>
                 </TouchableHighlight>
-                
                 <TouchableHighlight onPress={() =>{
-                    this.props.setModalVisible(true) 
+                    this.props.setModalVisible(true)
                     this.props.setParams({ this_position: this.props.this_position + 6, this_side: side});
-                }}> 
+                }}>
                     <View style={{width:40, height:19, backgroundColor:"rgba(46, 167, 224, 0.5)"}}/>
                 </TouchableHighlight>
             </View>
@@ -42,6 +41,6 @@ const styles = StyleSheet.create({
         marginLeft:17,
         marginRight:17,
         marginBottom:0,
-        justifyContent: 'space-between', 
+        justifyContent: 'space-between',
     },
 })
