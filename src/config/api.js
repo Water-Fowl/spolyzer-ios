970af54508a110346a69d4608e2da0806319af13
yamad07 <yuhsukeshootsfc@gmail.com>
2018-01-11 17:34:32 +0900

    [add] ポストするエンドポイントを追加

--- src/config/api.js
+++ src/config/api.js
@@ -3,3 +3,5 @@ export const BASE_URL = "http://52.198.150.98/api/v1";
 export const SIGN_IN_ENDPOINT = BASE_URL + "/auth/sign_in/"
 export const REGISTRATION_ENDPOINT = BASE_URL + "/auth/"
 export const POST_SCORE_GAME_ENDPOINT = BASE_URL + "/games/"
+export const GET_SCORES_ENDPOINT = BASE_URL + "/scores/"
+export const GET_GAMES_ENDPOINT = BASE_URL + "/games/"
