26972ba804b124c1c56dc527071d1e9d8f574bfd
yamad07 <yuhsukeshootsfc@gmail.com>
2018-01-20 20:28:14 +0900

    [add] Analysisの条件を設定するためのReducerを追加

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -16,15 +16,29 @@ export default function gameReducer(state=initialState, action={}){
       return Object.assign({}, state, {
         score_game_ids: action.score_game_ids
       })
-    case GAME_SETTING:
+    case GAME_TYPE_SETTING:
       return Object.assign({}, state, {
         game_type: action.game_type,
-        shot_type: action.shot_type,
-        term: actions.term,
-        opponent_user_id: action.opponent_user_id,
-        game_ids: action.game_ids
       })
-
+    case SHOT_TYPE_SETTING:
+      return Object.assign({}, state, {
+        shot_type: action.shot_type,
+      })
+    case TERM_SETTING:
+      return Object.assign({}, state, {
+        term: action.term
+      })
+    case OPPONENT_USER_SETTING:
+      return Object.assign({}, state, {
+        opponent_user_id: action.opponent_user_id
+      })
+    case GAME_SETTING_REQUST:
+      return state
+    case GAME_SETTING_RECEIVED:
+      return Object.assign({}, state, {
+        score_game_ids: action.score_game_ids
+      })
+    default:
+      return state
   }
-  return state
 }
