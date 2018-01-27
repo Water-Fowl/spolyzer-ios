454e0fd58eaf133531d6e11984b2c0457cf7cfe2
maysay526 <maysay526@gmail.com>
2018-01-27 19:24:23 +0900

    [add]対戦相手追加

--- src/scenes/analysis_view.js
+++ src/scenes/analysis_view.js
@@ -36,6 +36,26 @@ export default class AnalysisView extends React.Component{
                     複合分析結果
                 </Text>
 
+                <View style={{flexDirection:"row"}}>
+                    <Text　style={styles.analysis_view_vs}>
+                        vs
+                    </Text>
+
+                    <View style={styles.name_frame}>
+                        <View style={styles.opponent_name_frame}>
+                            <Image
+                            source={require('../../assets/img/score_creat_person.png')}
+                            style={ styles.person }
+                           />
+
+                            <Text style={styles.opponent_name}>
+                                池田　社長
+                            </Text>
+                        </View>
+
+                    </View>
+                </View>
+
 
 
 
@@ -66,5 +86,59 @@ const styles = StyleSheet.create({
         backgroundColor: 'transparent',
         fontWeight: 'bold',       
     },
+
+    analysis_view_vs:{
+        fontWeight: 'bold',
+        color:"skyblue",
+        fontSize:26,
+        alignSelf: 'flex-start',
+        marginTop: 16,
+        marginLeft: 210,
+        backgroundColor: "transparent",
+    },
+
+    name_frame: {
+        borderRightColor: '#0a2444',
+        borderTopColor: '#0a2444',
+        borderLeftColor: '#0a2444',
+        borderBottomColor: '#0a2444',
+        height: 30,
+        width: 110,
+        borderWidth: 1,
+        marginLeft: 6,
+        borderRadius: 4,
+        marginTop: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+     },
+
+    opponent_name_frame: {
+        borderRightColor: '#0a2444',
+        borderTopColor: '#0a2444',
+        borderLeftColor: '#0a2444',
+        borderBottomColor: '#0a2444',
+        height: 26,
+        width: 106,
+        borderWidth: 1,
+        borderRadius: 2,
+        flexDirection:"row"
+        
+     },
+
+     person: {
+        marginTop: 1,
+        marginLeft: 8,
+        height: 20,
+        width: 20,
+        opacity: 0.5,
+     },
+     opponent_name: {
+        backgroundColor: "transparent",
+        color: "#ffffff",
+        marginTop: 5,
+        marginLeft: 7,
+        fontWeight: 'bold', 
+        fontSize: 13,
+     }
     
 });
