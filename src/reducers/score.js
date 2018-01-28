c45a2aaedacd1a02e70a942b48efd900adaa596e
yamad07 <yuhsukeshootsfc@gmail.com>
2018-01-28 15:18:52 +0900

    [add] scoreReducersのindent修正

--- src/reducers/score.js
+++ src/reducers/score.js
@@ -1,52 +1,45 @@
 import{
-    ADD_SCORE,
-    SET_MODAL,
-    HIDE_MODAL,
-    SCORE_REQUEST,
-    SCORE_RECIEVED,
+  ADD_SCORE,
+  SET_MODAL,
+  HIDE_MODAL,
+  SCORE_REQUEST,
+  SCORE_RECIEVED,
 } from '../actions/score';
 
-// 初期化
 const initialState = {
-    positions: [],
-    actions: [],
-    sides: [],
-    modal: false,
+  positions: [],
+  actions: [],
+  sides: [],
+  modal: false,
 };
 
-// 入り口は一つにしてswitchで分ける。
-// stateは前回のstateもしくはinitialStateが
-// actionはactionでreturnしたvalueが渡る。
-// reducer内ではstateを更新することはせず、新しいstate(assignして)を返す。
 export default function scoreReducer(state = initialState, action = {}){
-  // typeでswitchする
-    switch (action.type){
-        case ADD_SCORE:
-      // 空のObnectにマージする
-            return Object.assign({}, state, {
-                actions: state.actions.concat([action.action]),
-                positions: state.positions.concat([state.position]),
-                sides: state.sides.concat([state.side]),
-            })
-        case SET_MODAL:
-            return Object.assign({}, state, {
-                modal : action.modal,
-                position: action.position,
-                side: action.side
-            })
-        case HIDE_MODAL:
-            return Object.assign({}, state, {
-                modal : action.modal,
-            })
+  switch (action.type){
+    case ADD_SCORE:
+      return Object.assign({}, state, {
+        actions: state.actions.concat([action.action]),
+        positions: state.positions.concat([state.position]),
+        sides: state.sides.concat([state.side]),
+      })
+    case SET_MODAL:
+      return Object.assign({}, state, {
+        modal : action.modal,
+        position: action.position,
+        side: action.side
+      })
+    case HIDE_MODAL:
+      return Object.assign({}, state, {
+        modal : action.modal,
+      })
 
-        case SCORE_REQUEST:
-            return state
+    case SCORE_REQUEST:
+      return state
 
-        case SCORE_RECIEVED:
-            return Object.assign({}, state, {
-                current_score_game_id: action.current_score_game_id
-            })
-        default:
-            return state;
-    }
+    case SCORE_RECIEVED:
+      return Object.assign({}, state, {
+        current_score_game_id: action.current_score_game_id
+      })
+    default:
+      return state;
+  }
 }
