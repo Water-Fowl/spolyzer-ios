51334b5e6b1ca2c84dee09ae039eb20025b5de7b
楡井雄志朗 <yushiro_nirel@keio.jp>
2018-02-09 19:14:31 +0900

    [add]Completeボタンの追加

--- src/containers/profile/profile_edit/index.js
+++ src/containers/profile/profile_edit/index.js
@@ -70,7 +70,7 @@ class ProfileEdit extends React.Component {
 
 
       <View style={{flex:1,}}>
-      <Text style={styles.top_bar}>中</Text>
+          <NavigateButton action={Actions.mypage_top} style={styles.complete} text="Complete" />
       </View>
       </View>
     );
@@ -125,99 +125,8 @@ const styles = StyleSheet.create({
     marginTop:10,
   },
 
-  subtitle_text: {
-    color: '#ffffff',
-    fontSize: 22,
+  complete: {
     alignSelf: 'center',
-    marginTop: -28,
-    backgroundColor: 'transparent',
-    fontWeight: 'bold',
-  },
-
-  game_style_text: {
-    color: '#ffffff',
-    fontSize: 15,
-    marginTop: 45,
-    marginLeft: 40,
-    backgroundColor: 'transparent',
-    fontWeight: 'bold',
-    alignSelf: 'flex-start',
-  },
-
-  shot_type_text: {
-    color: '#ffffff',
-    fontSize: 15,
-    marginTop: 68,
-    marginLeft: 40,
-    backgroundColor: 'transparent',
-    fontWeight: 'bold',
-    alignSelf: 'flex-start',
-  },
-
-  term_text: {
-    color: '#ffffff',
-    fontSize: 15,
-    marginTop: 30,
-    marginLeft: 40,
-    backgroundColor: 'transparent',
-    fontWeight: 'bold',
-    alignSelf: 'flex-start',
-  },
-
-  opponent_text: {
-    color: '#ffffff',
-    fontSize: 15,
-    marginTop: 32,
-    marginLeft: 40,
-    backgroundColor: 'transparent',
-    fontWeight: 'bold',
-    alignSelf: 'flex-start',
-    marginRight: 22,
-  },
-
-  opponent_frame: {
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    borderRightColor: '#0a2444',
-    borderTopColor: '#0a2444',
-    borderLeftColor: '#0a2444',
-    borderBottomColor: '#0a2444',
-    height: 34,
-    width: 108,
-    borderWidth: 1.5,
-    marginLeft: 6,
-    borderRadius: 3,
-    marginTop: 25,
-  },
-
-  game_select_text: {
-    color: '#ffffff',
-    fontSize: 15,
-    marginTop: 36,
-    marginLeft: 40,
-    backgroundColor: 'transparent',
-    fontWeight: 'bold',
-    alignSelf: 'flex-start',
-  },
-
-  game_select_frame: {
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    borderRightColor: '#0a2444',
-    borderTopColor: '#0a2444',
-    borderLeftColor: '#0a2444',
-    borderBottomColor: '#0a2444',
-    height: 34,
-    width: 222,
-    borderWidth: 1.5,
-    marginLeft: 28,
-    borderRadius: 3,
-    marginTop: 25,
-  },
-
-  analyze: {
-    alignSelf: 'center',
-    marginTop: 48,
   },
 
 });
