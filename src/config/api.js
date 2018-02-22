c17a2f5bafc79c567b6b137b7842c7b049c11413
yamad07 <yuhsukeshootsfc@gmail.com>
2018-02-23 01:03:38 +0900

    [add] users get のエンドポイントを追加

--- src/config/api.js
+++ src/config/api.js
@@ -2,6 +2,7 @@ export const BASE_URL = "http://52.198.150.98/api/v1";
 export const LOCAL_URL = "http://localhost:3000/api/v1";
 
 export const SIGN_IN_ENDPOINT = `${LOCAL_URL}/auth/sign_in/`;
+export const GET_USER_ENDPOINT = `${LOCAL_URL}/users/`;
 export const REGISTRATION_ENDPOINT = `${LOCAL_URL}/auth/`;
 export const POST_SCORE_GAME_ENDPOINT = `${LOCAL_URL}/games/`;
 export const GET_SCORES_ENDPOINT = `${LOCAL_URL}/scores/`;
