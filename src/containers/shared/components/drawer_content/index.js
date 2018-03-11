d2efc90589cf65b0d147aaa7293e139a68e4b6d0
maysay526 <maysay526@gmail.com>
2018-03-11 16:44:30 +0900

    [add]drawerの中身の作成

--- src/containers/shared/components/drawer_content/index.js
+++ src/containers/shared/components/drawer_content/index.js
@@ -11,21 +11,139 @@ export default class DrawerContent extends React.Component {
 
   render() {
     return (
-    	<View style={styles.container}>
-        <Text　style={styles.logout}>
-          ログアウト
-        </Text>
+    	<View style={styles.drawerContainer}>
+        <View style={styles.userContainer}>
+          <Image
+            source={require("../../../../assets/img/my_page_user_icon.png")}
+            style={styles.userIcon}
+          />
+          <Text　style={styles.userName}>
+            Ikeda Yoshiki
+          </Text>
+        </View>
+        <View style={styles.sportContainer}>
+          <View style={styles.kyugiContainer}>
+            <View style={styles.circle} />
+            <Text style={styles.kyogi}>
+              競技
+            </Text>
+          </View>
+          <Text style={styles.bad}>
+            バドミントン
+          </Text>
+        </View>
+        <View style={styles.contactContainer}>
+          <Image
+            source={require("../../../../assets/img/contact.png")}
+            style={styles.contactImage}
+          />
+          <Text style={styles.contactText}>
+            お問い合わせ
+          </Text>
+        </View>
+        <View style={styles.logoutContainer}>
+          <Image
+            source={require("../../../../assets/img/logout.png")}
+            style={styles.logoutImage}
+          />
+          <Text style={styles.logoutText}>
+            ログアウト
+          </Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-
+  drawerContainer: {
+    flex: 1,
+    backgroundColor: "black"
   },
-  logout: {
-    fontSize: 16,
-    marginTop: 30
+  userContainer: {
+    borderBottomColor: "#2EA7E0",
+    borderWidth: 3,
+    flexDirection: "row"
+  },
+  userIcon: {
+    marginTop: 12,
+    marginLeft: 6,
+    marginBottom: 6,
+    width: 50,
+    height: 50
+  },
+  userName: {
+    marginTop: 38,
+    marginLeft: 8,
+    color: "white",
+    fontSize: 20
+  },
+  sportContainer: {
+    height: 270,
+    borderBottomColor: "#2EA7E0",
+    borderWidth: 1
+  },
+  kyugiContainer: {
+    borderBottomColor: "#2EA7E0",
+    borderWidth: 1,
+    flexDirection: "row"
+  },
+  circle: {
+    width: 16,
+    height: 16,
+    borderRadius: 10,
+    backgroundColor: "#2EA7E0",
+    marginTop: 20,
+    marginLeft: 10
+  },
+  kyogi: {
+    marginTop: 16,
+    marginLeft: 8,
+    color: "white",
+    fontSize: 22,
+    marginBottom: 2
+  },
+  bad: {
+    marginTop: 14,
+    marginLeft: 10,
+    color: "white",
+    fontSize: 20,
+    marginBottom: 4
+  },
+  contactContainer: {
+    borderBottomColor: "#2EA7E0",
+    borderWidth: 1,
+    flexDirection: "row"
+  },
+  contactImage: {
+    width: 22,
+    height: 16,
+    marginTop: 22,
+    marginLeft: 8
+  },
+  contactText: {
+    marginTop: 20,
+    marginLeft: 6,
+    color: "white",
+    fontSize: 21,
+    marginBottom: 2
+  },
+  logoutContainer: {
+    borderBottomColor: "#2EA7E0",
+    borderWidth: 1,
+    flexDirection: "row"
+  },
+  logoutImage: {
+    width: 20,
+    height: 23,
+    marginTop: 18,
+    marginLeft: 10
+  },
+  logoutText: {
+    marginTop: 20,
+    marginLeft: 6,
+    color: "white",
+    fontSize: 21,
+    marginBottom: 2
   }
 });
