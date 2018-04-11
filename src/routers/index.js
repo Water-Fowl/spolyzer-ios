89bb170d2ef1ff720543611dc0589686078b4a80
yamad07 <yuhsukeshootsfc@gmail.com>
2018-04-11 15:05:47 +0900

    [fix] UserListに関するbagfix

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import configureStore from "../stores";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider, connect } from "react-redux";
+import {
+  Text
+} from "react-native";
 
 import Route from "./routes";
 const { persistor, store } = configureStore();
@@ -10,7 +13,7 @@ export default class ReduxRoute extends React.Component{
   render(){
     return(
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
+        <PersistGate loading={<Text>読み込み中</Text>} persistor={persistor}>
           <Route />
         </PersistGate>
       </Provider>
