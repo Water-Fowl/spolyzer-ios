cfaabc33b2026e5ae79bfa794274a656535245e0
kryoi <kryoi@kryoinoMacBook-puro.local>
2018-04-12 23:34:50 +0900

    [fix] react-native linkの実行

--- android/app/src/main/java/com/spolyzer/MainApplication.java
+++ android/app/src/main/java/com/spolyzer/MainApplication.java
@@ -3,6 +3,7 @@ package com.spolyzer;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new SplashScreenReactPackage(),
             new PickerPackage(),
             new ImagePickerPackage(),
