f2ad5339e96859d730a65298fcab78e155edc4c8
maysay526 <maysay526@gmail.com>
2018-04-26 11:50:21 +0900

    [add]ioskitのスタイルを書いた

--- src/routers/index.js
+++ src/routers/index.js
@@ -5,15 +5,29 @@ import { Provider, connect } from "react-redux";
 import {
   Text
 } from "react-native";
+import { ThemeProvider } from "react-native-ios-kit";
+import {
+  $spolyzerBlue,
+  $spolyzerDarkBlue,
+  $transparent
+} from "../const/color";
 
 import Route from "./routes";
 const store = configureStore();
+const theme = {
+  barColor: $transparent,
+  textColor: "white",
+  placeholderColor: "white",
+  footnoteColor: $spolyzerDarkBlue
+};
 
 export default class ReduxRoute extends React.Component{
   render(){
     return(
       <Provider store={store}>
-        <Route />
+        <ThemeProvider theme={theme}>
+          <Route />
+        </ThemeProvider>
       </Provider>
     );
   }
