3f71a2904a15bddbbb1934787d4f2330f363de1a
maysay526 <maysay526@gmail.com>
2018-04-26 11:50:00 +0900

    [add]DashBoardCreateのページの中身を書いた

--- src/containers/templates/DashboardCreate.js
+++ src/containers/templates/DashboardCreate.js
@@ -15,14 +15,98 @@ import {
   NavigateButton
 } from "atoms";
 import {
-  $spolyzerDarkBlue
+  NavigationRow,
+  SegmentedControl
+} from "react-native-ios-kit";
+import {
+  $spolyzerBlue,
+  $spolyzerDarkBlue,
+  $transparent
 } from "../../const/color";
 
 class DashboardCreate extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedIndex: 0
+    };
+  }
+  setSegment(value, index) {
+    this.setState({
+      selectedValue: value,
+      selectedIndex: index
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.segmentedControlContainer}>
+          <SegmentedControl
+            values={["シングルス", "ダブルス"]}
+            selectedIndex={this.state.selectedIndex}
+            onValueChange={(value, index) =>
+              this.setState({
+                selectedValue: value,
+                selectedIndex: index
+              })
+            }
+            tintColor={$spolyzerBlue}
+            style={{ width: 222, alignSelf: "center" }}
+          />
+        </View>
+        <View style={styles.listContainer}>
+          <NavigationRow
+            title="対戦相手１"
+            onPress={() => {Actions.DashboardTop();}}
+            info="池田吉来"
+            theme={{
+              backgroundColor: $transparent
+            }}
+          />
+          <NavigationRow
+            title="対戦相手２"
+            onPress={() => {Actions.DashboardTop();}}
+            info="吉川明成"
+            theme={{
+              backgroundColor: $transparent
+            }}
+          />
+          <NavigationRow
+            title="目標設定"
+            onPress={() => {Actions.DashboardTop();}}
+            info="ミス"
+            theme={{
+              backgroundColor: $transparent
+            }}
+          />
+          <NavigationRow
+            title="球種"
+            onPress={() => {Actions.DashboardTop();}}
+            info="ヘアピン"
+            theme={{
+              backgroundColor: $transparent
+            }}
+          />
+          <NavigationRow
+            title="エリア"
+            onPress={() => {Actions.DashboardTop();}}
+            info="A,B"
+            theme={{
+              backgroundColor: $transparent
+            }}
+          />
+          <NavigationRow
+            title="目標回数"
+            onPress={() => {Actions.DashboardTop();}}
+            info="４"
+            theme={{
+              backgroundColor: $transparent
+            }}
+          />
+        </View>
+        <View style={styles.creatingButtonContainer}>
+          <NavigateButton style={styles.navigateButton} action={() => {Actions.DashboardTop();}} text="作成" />
+        </View>
       </View>
     );
   }
@@ -33,5 +117,18 @@ export default connect(mapStateToProps)(baseEnhancer(DashboardCreate));
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  segmentedControlContainer: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  listContainer: {
+    flex: 3,
+    borderTopColor: $spolyzerDarkBlue,
+    borderWidth: 1
+  },
+  creatingButtonContainer: {
+    flex: 2,
+    justifyContent: "center"
   }
 });
