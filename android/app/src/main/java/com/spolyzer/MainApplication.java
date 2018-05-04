cc198b0a6c7c0a9ca9c913979e0d59a312923fb3
axutuzaxa <axutuzaxa@ikedakichirai-no-MacBook-Air.local>
2018-05-04 14:27:25 +0900

    [add] react-native-configのインストール

--- android/app/src/main/java/com/spolyzer/MainApplication.java
+++ android/app/src/main/java/com/spolyzer/MainApplication.java
@@ -3,6 +3,7 @@ package com.spolyzer;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
             new SplashScreenReactPackage(),
             new PickerPackage(),
             new ImagePickerPackage(),
