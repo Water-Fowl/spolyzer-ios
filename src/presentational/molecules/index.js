f58cb936dfc124a30b6d533077a324e0bbafe24a
maysay526 <maysay526@gmail.com>
2018-05-06 17:23:37 +0900

    [change]DashboardCardとしてmolecules化

--- src/presentational/molecules/index.js
+++ src/presentational/molecules/index.js
@@ -4,9 +4,11 @@
 import UserListItem from "./UserListItem";
 import SexPicker from "./SexPicker";
 import ShotTypeModal from "./ShotTypeModal";
+import DashboardCard from "./DashboardCard";
 
 export {
   UserListItem,
   SexPicker,
-  ShotTypeModal
+  ShotTypeModal,
+  DashboardCard
 };
