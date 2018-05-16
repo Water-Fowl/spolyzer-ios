d0206c4786243b3a18a3771f384bf3d4d3958e00
kryoi <kryoi@kryoinoMacBook-puro.local>
2018-05-16 11:37:57 +0900

    [fix] sport_id別にデータを表示させるようにした

--- src/containers/templates/GameAnalysisCreate.js
+++ src/containers/templates/GameAnalysisCreate.js
@@ -1,7 +1,13 @@
 import React from "react";
 import templateEnhancer from "./hoc";
 import { ActionConst, Actions } from "react-native-router-flux";
-import { View, StyleSheet, FlatList, Text, TouchableOpacity } from "react-native";
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Text,
+  TouchableOpacity
+} from "react-native";
 import { connect } from "react-redux";
 import { TopContentBar, TextBox } from "atoms";
 import { Icon, SegmentedControl } from "react-native-ios-kit";
@@ -16,7 +22,6 @@ import {
 import { mapStateToProps, timeEncode } from "utils";
 
 class GameAnalysisCreate extends React.Component {
-
   constructor(props) {
     super(props);
     this.getUsersGamesEvent.bind(this);
@@ -63,15 +68,15 @@ class GameAnalysisCreate extends React.Component {
         gameModules.getShotTypeCountsReceived
       )
     );
-    Actions.GameAnalysisView({ "games": item });
+    Actions.GameAnalysisView({ games: item });
   }
 
   setListData() {
     let listData = [];
     if (!this.state.games) return listData;
-    for (let game of this.state.games.slice().reverse()) {
-      if (game.left_users.length === this.state.selectedIndex + 1)
-        listData.push(game);
+    for (let gameData of this.state.games.slice().reverse()) {
+      if (gameData.left_users.length === this.state.selectedIndex + 1 && gameData.game.sport_id===this.props.sport.id)
+        listData.push(gameData);
     }
     return listData;
   }
@@ -88,7 +93,6 @@ class GameAnalysisCreate extends React.Component {
 
       if (right_users[index].name === this.props.profile.userName)
         opponentUsers.side = 1;
-
     }
     return opponentUsers.side
       ? opponentUsers.left.join(" / ")
