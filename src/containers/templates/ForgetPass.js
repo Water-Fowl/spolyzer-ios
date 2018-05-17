3280511841d60775e0153fd21161d34510f2f115
maysay526 <maysay526@gmail.com>
2018-05-17 14:44:02 +0900

    [add]パスワードを忘れた場合の２つの画面を作った。

---
+++ src/containers/templates/ForgetPass.js
@@ -0,0 +1,101 @@
+import Orientation from "react-native-orientation";
+import React from "react";
+import templateEnhancer from "./hoc";
+import { Actions } from "react-native-router-flux";
+import {
+  StyleSheet,
+  Image,
+
+  Text, TextInput, TouchableOpacity, View
+} from "react-native";
+import { connect } from "react-redux";
+import { mapStateToProps } from "utils";
+
+class ForgetPass extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/img/spolyzer_top.png")}
+        />
+        <Text style={styles.text}>
+          パスワード再設定メールを送る
+        </Text>
+        <View style={styles.form}>
+          <TextInput
+            onChangeText={email => this.setState({ email })}
+            placeholder="メールアドレス"
+            placeholderTextColor="#666677"
+            style={styles.textField}
+            keyboardType="email-address"
+            returnKeyType="done"
+          />
+        </View>
+        <View style={styles.button}>
+          <TouchableOpacity onPress={Actions.ForgetPassDone}>
+            <Text style={styles.buttonText}>
+              送信
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+
+export default connect(mapStateToProps)(templateEnhancer(ForgetPass));
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  logo: {
+    marginTop: 80,
+    marginBottom: 70,
+    alignSelf: "center"
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
+    backgroundColor: "transparent",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  form: {
+    borderColor: "#28a8de",
+    height: 42,
+    width: "80%",
+    borderWidth: 1.3,
+    alignSelf: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+    marginTop: 50
+  },
+  textField: {
+    fontSize: 20,
+    color: "#ffffff",
+    paddingLeft: 12,
+    letterSpacing: 0
+  },
+  button: {
+    borderColor: "#28a8de",
+    height: 42,
+    width: "80%",
+    borderWidth: 1.3,
+    alignSelf: "center",
+    borderRadius: 5,
+    marginTop: 60,
+    justifyContent: "center"
+  },
+  buttonText: {
+    color: "#28a8de",
+    textAlign: "center",
+    fontSize: 19,
+    marginTop: 1,
+    marginBottom: 1,
+    backgroundColor: "transparent"
+  }
+});
---
+++ src/containers/templates/ForgetPassDone.js
@@ -0,0 +1,82 @@
+import Orientation from "react-native-orientation";
+import React from "react";
+import templateEnhancer from "./hoc";
+import { Actions } from "react-native-router-flux";
+import {
+  StyleSheet,
+  Image,
+
+  Text, TextInput, TouchableOpacity, View
+} from "react-native";
+import { connect } from "react-redux";
+import { mapStateToProps } from "utils";
+
+class ForgetPassDone extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/img/spolyzer_top.png")}
+        />
+        <Text style={styles.mainText}>
+          パスワード再設定メール送信しました
+        </Text>
+        <View style={styles.textContainer}>
+          <Text style={styles.subText}>
+            **********************にメールを送信しました。メールに記載されたリンクをクリックするとパスワードをリセットできます。
+            {"\n"}{"\n"}メールが届かない場合、迷惑メールやスパムフォルダーなどもご確認ください。
+          </Text>
+        </View>
+        <TouchableOpacity onPress={() => {Actions.popTo("login");}}>
+          <Text style={styles.actionText}>
+            ログイン
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+
+export default connect(mapStateToProps)(templateEnhancer(ForgetPassDone));
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  logo: {
+    marginTop: 80,
+    marginBottom: 70,
+    alignSelf: "center"
+  },
+  mainText: {
+    color: "white",
+    fontSize: 19,
+    backgroundColor: "transparent",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  textContainer: {
+    marginTop: 60,
+    width: "90%",
+    alignSelf: "center"
+  },
+  subText: {
+    color: "white",
+    fontSize: 12,
+    backgroundColor: "transparent",
+    fontWeight: "bold",
+    lineHeight: 18
+  },
+  actionText: {
+    color: "#28a8de",
+    textDecorationLine: "underline",
+    textDecorationColor: "#28a8de",
+    marginTop: 40,
+    marginLeft: 20,
+    fontSize: 16,
+    backgroundColor: "transparent"
+  }
+});
